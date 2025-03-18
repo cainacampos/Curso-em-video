fd53aba4c42d1960a3ca609e8224866f19ca0d18
Cainã Campos <campos.caina@gmail.com>
2025-03-18 17:31:50 -0300

    Estudo de hoje. 18/03/2025

---
+++ Python 3 – Mundo 1 [40 Horas]/Exercícios/ex027.py
@@ -0,0 +1,8 @@
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split() 
+'''Essa função split transforma uma frase ou nome em uma 
+lista com o seu conteudo. Cainã Campos se
+transforma em dois elementos ['Cainã', 'Campos']
+Ele vai fatiar'''
+print('Primeiro nome: {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[len(nome)-1]))
